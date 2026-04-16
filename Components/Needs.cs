611b80cfbde1c5be6b23cffb27bf54e3f44f50b5
bitpunkxr <tony69elvis@gmail.com>
2026-04-16 13:54:51 +0100

    Made property card and image card fully reusable in other pages. about to start the info and section

---
+++ Components/Needs.cs
@@ -0,0 +1,8 @@
+public class DisplayCard
+{
+    public string? Category { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+}
---
+++ Components/Properties.cs
@@ -0,0 +1,10 @@
+public class HomeCard
+{
+    public string? Name { get; set; }
+
+    public string? Location { get; set; }
+
+    public int Price { get; set; }
+        
+    public int Review { get; set; }
+}
