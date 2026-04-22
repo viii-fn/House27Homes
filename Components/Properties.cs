de48e43d4eb7cf88b117e39dde7464a4056a4730
bitpunkxr <tony69elvis@gmail.com>
2026-04-22 23:59:15 +0100

    Building Property Details Drawer with styled component and fixed previoue 10 bugs

--- Components/Properties.cs
+++ Components/Properties.cs
@@ -1,8 +1,10 @@
-public class HomeCard
+public class Properties
 {
     public string? Name { get; set; }
 
     public string? Location { get; set; }
+    
+    public string? Description { get; set; }
 
     public int Price { get; set; }
         
